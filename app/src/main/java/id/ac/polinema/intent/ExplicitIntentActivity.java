80ddbf2eaa2aac4106d75592f8bfdc018e78eabb
HattaMaulana <mahatta.maulana@gmail.com>
2020-02-17 13:41:06 +0700

    Melengkapi Pertanyaan Explicit Intent

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,28 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText edtName;
+    private TextView tvOutput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        edtName = findViewById(R.id.input_name);
+        tvOutput = findViewById(R.id.text_output);
     }
 
+    public void clickSubmit(View view) {
+        String name = edtName.getText().toString();
+        tvOutput.setText("Hello "+ name +", Congratulations");
+    }
 }
