a8ee8781e028a5df353fbfeee405f60c3a54fb29
HattaMaulana <mahatta.maulana@gmail.com>
2020-02-17 14:36:40 +0700

    Praktikum Implicit Intent

--- app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
@@ -1,8 +1,15 @@
 package id.ac.polinema.intent;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,5 +31,25 @@ public class ImplicitIntentActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED) return;
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (data != null) {
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap factory = BitmapFactory.decodeStream(
+                            getContentResolver().openInputStream(imageUri));
+                    avatarImage.setImageBitmap(factory);
+                } catch (Exception e) {
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void handleChangeAvatar(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
     }
 }
